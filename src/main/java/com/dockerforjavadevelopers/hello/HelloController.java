3ba1c78d82cd31e43063da143110fc254e2d4beb
hashilbasheer <hashilbasheer@gmail.com>
2023-04-15 16:46:19 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -1,3 +1,5 @@
+package com.dockerforjavadevelopers.hello;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
