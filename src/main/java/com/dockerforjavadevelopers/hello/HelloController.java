5464d2474c3b9e2c0377d9e634626bb32d88eddd
hashilbasheer <hashilbasheer@gmail.com>
2023-04-15 17:32:24 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -1,16 +1,15 @@
 package com.dockerforjavadevelopers.hello;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
     
-    @GetMapping("/")
-    public ResponseEntity<String> index() {
-        return new ResponseEntity<>("Success", HttpStatus.OK);
+    @RequestMapping("/")
+    public String index() {
+        return "Welcome to java api service\n";
     }
   
     
