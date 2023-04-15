72ef8be77a0470929a14c2029cc9cb2297ea6b8a
hashilbasheer <hashilbasheer@gmail.com>
2023-04-15 20:27:35 +0530

    Update Application.java

--- src/main/java/com/dockerforjavadevelopers/hello/Application.java
+++ src/main/java/com/dockerforjavadevelopers/hello/Application.java
@@ -1,38 +1,15 @@
 package com.dockerforjavadevelopers.hello;
 
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
 
-public class HelloWorld {
-    public static void main(String[] args) throws Exception {
-        // Read the GITHUB_RUN_NUMBER environment variable
-        String githubRunNumber = System.getenv("GITHUB_RUN_NUMBER");
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-        // Create a new HTTP server
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+@SpringBootApplication
+public class Application {
 
-        // Define a handler for the root path
-        server.createContext("/", new HttpHandler() {
-            public void handle(HttpExchange exchange) throws IOException {
-                String response = "Hello World! GitHub run number: " + githubRunNumber;
-
-                // Send the response headers
-                exchange.getResponseHeaders().set("Content-Type", "text/plain");
-                exchange.sendResponseHeaders(200, response.getBytes().length);
-
-                // Send the response body
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-            }
-        });
-
-        // Start the server
-        server.start();
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
+
 }
