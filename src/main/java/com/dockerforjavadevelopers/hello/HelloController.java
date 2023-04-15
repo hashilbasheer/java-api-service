d6dfb590d057506b285e32785ca71d38cd76d52a
hashilbasheer <hashilbasheer@gmail.com>
2023-04-15 20:13:11 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -1,16 +1,18 @@
 package com.dockerforjavadevelopers.hello;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/")
-    public String index() {
-        return "Hello World\n";
+    @Value("${GITHUB_RUN_NUMBER}")
+    private String githubRunNumber;
+
+    @GetMapping("/")
+    public String hello() {
+        String response = "Hello World! GitHub run number: " + githubRunNumber;
+        return response;
     }
-  
-    
 }
