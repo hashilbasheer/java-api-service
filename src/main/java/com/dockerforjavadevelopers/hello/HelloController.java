8807b01e90244356ef9e561262de15797b1fb269
hashilbasheer <hashilbasheer@gmail.com>
2023-04-15 19:46:33 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -1,59 +1,16 @@
 package com.dockerforjavadevelopers.hello;
 
 
-package com.example.demo;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class Controller {
-
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World\n";
     }
-
-    @GetMapping("/json")
-    public User getUser() {
-        User user = new User();
-        user.setName("John Doe");
-        user.setEmail("john.doe@example.com");
-        user.setAge(30);
-        return user;
-    }
-
+  
+    
 }
-
-class User {
-    private String name;
-    private String email;
-    private int age;
-
-    // Getters and setters
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
-
