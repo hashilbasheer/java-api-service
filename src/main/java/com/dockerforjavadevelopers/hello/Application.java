8334f2f6ef0006ff8322daa5d05106dd823084a3
hashilbasheer <hashilbasheer@gmail.com>
2023-04-15 20:12:28 +0530

    Update Application.java

--- src/main/java/com/dockerforjavadevelopers/hello/Application.java
+++ src/main/java/com/dockerforjavadevelopers/hello/Application.java
@@ -1,20 +1,38 @@
 package com.dockerforjavadevelopers.hello;
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-public class Application {
-    
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        // Read the GITHUB_RUN_NUMBER environment variable
+        String githubRunNumber = System.getenv("GITHUB_RUN_NUMBER");
+
+        // Create a new HTTP server
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        // Define a handler for the root path
+        server.createContext("/", new HttpHandler() {
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Hello World! GitHub run number: " + githubRunNumber;
+
+                // Send the response headers
+                exchange.getResponseHeaders().set("Content-Type", "text/plain");
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+
+                // Send the response body
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+
+        // Start the server
+        server.start();
     }
-
 }
