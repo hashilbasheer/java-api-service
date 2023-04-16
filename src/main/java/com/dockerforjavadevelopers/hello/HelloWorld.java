05f7ceb8572ee1ccabc28b1eb253c94f0c842114
hashilbasheer <hashilbasheer@gmail.com>
2023-04-16 12:30:19 +0530

    Update HelloWorld.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloWorld.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloWorld.java
@@ -10,29 +10,23 @@ import com.sun.net.httpserver.HttpServer;
 
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
-        // Read the GITHUB_RUN_NUMBER environment variable
-        String githubRunNumber = args.length > 0 ? args[0] : "unknown";
+        String githubRunNumber = System.getenv("GITHUB_RUN_NUMBER");
 
-        // Create a new HTTP server
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        // Define a handler for the root path
         server.createContext("/", new HttpHandler() {
             public void handle(HttpExchange exchange) throws IOException {
                 String response = "Hello World! GitHub run number: " + githubRunNumber;
 
-                // Send the response headers
                 exchange.getResponseHeaders().set("Content-Type", "text/plain");
                 exchange.sendResponseHeaders(200, response.getBytes().length);
 
-                // Send the response body
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
                 os.close();
             }
         });
 
-        // Start the server
         server.start();
     }
 }
